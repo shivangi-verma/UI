789eec5b31c4e51d8a07f24a8ad9434694f9bb34
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-03 12:28:33 +0530

    Added Animation

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-import { animate } from "https://cdn.jsdelivr.net/npm/motion@12.23.24/+esm";
+import {
+  animate,
+  stagger,
+} from "https://cdn.jsdelivr.net/npm/motion@12.23.24/+esm";
 
 animate(".flower", { scale: 1 }, { ease: "ease", duration: 1 });
 animate(
@@ -23,3 +26,25 @@ animate(
   { y: [200, 0], opacity: [0, 1] },
   { ease: "ease", duration: 1 }
 );
+// staggerer
+
+// animate(
+//   // selector
+//   ".meet-person ",
+//   //   css properties
+//   {
+//     opacity: 1,
+//     y: [50, 0],
+//   },
+//   //   animation params
+//   {
+//     // ease: "bounce",
+//     duration: 1,
+//     delay: stagger(0.05),
+//   }
+// );
+// pehle acha ara tha ab kyu ni ara
+// stagger bounce hoke
+
+animate(".meet-person ", { opacity: 1, y: [50, 0] }, { delay: stagger(0.05) });
+animate(".position-job ", { opacity: 1, y: [50, 0] }, { delay: stagger(0.05) });
